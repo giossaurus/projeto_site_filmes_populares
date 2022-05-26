aba4f34e0d887ffe39e492f8b06567fb0ffe676f
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-05-26 11:47:42 -0300

    script movies

---
+++ main.js
@@ -0,0 +1,39 @@
+const moviesContainer = document.querySelector('movies')
+
+const movies = [
+    {
+        image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jp',
+        title: 'Batman',
+        rating: 9.8,
+        year: 2022,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        isFavorited: false
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
+        title: 'Doctor Strange',
+        rating: 6.2,
+        year: 2022,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        isFavorited: true
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
+        title: 'Avengers',
+        rating: 7.2,
+        year: 2019,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        isFavorited: false
+      },
+
+
+
+
+
+
+
+
+
+
+
+]
