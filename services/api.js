2b7a3b4a413001a3b3b84785dd921646c96a59ed
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-06-02 00:55:05 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,14 +1,14 @@
 import { apiKey } from "../environment/key.js"
 
 async function searchMovieByName(title) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`
+  const url = `https://api.themoviedb.org/3/movie/550?api_key=43b2ca0804dc1c18a7ec55867bd48014`
   const fetchResponse = await fetch(url)
   const { results } = await fetchResponse.json()
   return results
 }
 
 async function getPopularMovies() {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+  const url = `https://api.themoviedb.org/3/movie/550?api_key=43b2ca0804dc1c18a7ec55867bd48014`
   const fetchResponse = await fetch(url)
   const { results } = await fetchResponse.json()
   return results
